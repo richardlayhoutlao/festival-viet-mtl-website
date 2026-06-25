32d6ec55b941bdfaf871db914d57f5d6e0372dfd
ketchup383 <vincentduong383@gmail.com>
2026-06-25 15:36:09 -0400

    Updated proxy.ts to access Sanity-Studio properly

--- proxy.ts
+++ proxy.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing"
 export default createMiddleware(routing)
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  matcher: ['/', '/(vi|en|fr)/:path*', '/((?!api|_next|_vercel|studio|sanity|.*\\..*).*)'],
 }
