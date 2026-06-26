4429d7903cff14b9669f31425c46884aff53b676
ketchup383 <vincentduong383@gmail.com>
2026-06-26 16:23:31 -0400

    Added performer Schema to Sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,9 +2,10 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { vendorSchema } from './food-vendors'
 import { faqSchema } from './faq'
 import { eventSchema } from './event'
+import { performerSchema } from './performer'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [vendorSchema, faqSchema, eventSchema], 
+  types: [vendorSchema, faqSchema, eventSchema, performerSchema], 
 }
 
-export const schemaTypes = [vendorSchema, faqSchema, eventSchema]
+export const schemaTypes = [vendorSchema, faqSchema, eventSchema, performerSchema]
---
+++ sanity/schemaTypes/performer.ts
@@ -0,0 +1,34 @@
+export const performerSchema = {
+    name: 'performer',
+    title: 'Festival Performer',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Performer',
+            type: 'string',
+            description: 'The official name of the performer or group.',
+        },
+        {
+            name: 'artType',
+            title: 'Type of Art',
+            type: 'string',
+            description: 'What kind of performance do they do?',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+            description: 'A description of the performance.',
+        },
+        {
+            name: 'image',
+            title: 'Performer Photo',
+            type: 'image',
+            description: 'Upload a promotional photo of the performer.',
+            options: {
+                hotspot: true, 
+            }
+        }
+    ]
+}
